f447eb185dc1fc81a27bea909caff056a2ffa213
DrWatson <no@mail>
2021-10-17 15:28:49 -0400

    First real commit

--- README.md
+++ README.md
@@ -1,5 +1,21 @@
 # EHTGRMHDCal
 
+
+# Reproducing the environment
+**Note I am assuming you have at least julia 1.6 installed**
+
+Additionally you will require ehtim to be installed in a system aware python. Currently I am just using the build in fft so `pip install ehtim`
+should be enough. 
+
+If for some reason Julia is looking at the wrong python installation please open a Julia REPL and type
+```
+ENV["PYTHON"] = "/path/to/python/binary"
+using Pkg; Pkg.build()
+```
+This should allow you to use the custom python installation.
+
+
+
 This code base is using the Julia Language and [DrWatson](https://juliadynamics.github.io/DrWatson.jl/stable/)
 to make a reproducible scientific project named
 > EHTGRMHDCal
@@ -18,3 +34,15 @@ To (locally) reproduce this project, do the following:
 
 This will install all necessary packages for you to be able to run the scripts and
 everything should work out of the box, including correctly finding local paths.
+
+# Using the script
+The main script you should look at in this repo is src/main.jl
+To use it do something like
+```
+julia -p 2 main.jl filelist --data ../data/hops_3599_SGRA_LO_netcal_LMTcal_normalized_10s_preprocessed_snapshot_60_noisefrac0.05_scan252.uvfits --pa 90 --out test.csv  --stride 200
+```
+
+The only argument that isn't optional is `filelist`. This is a file that contains the paths of all the hdf5 file you
+would like to analyze. For the other options please see the docstring of the main function.
+
+**Note this will seem to hang at the begining. This is because Julia uses a JIT compiler which means it is called just before its first called**
---
+++ src/rose_optimizer.jl
@@ -0,0 +1,139 @@
+using ROSESoss
+using BlackBoxOptim
+import Distributions as Dists
+using HypercubeTransform
+using CSV
+using DataFrames
+using NamedTupleTools
+const fwhmfac = 2*sqrt(2*log(2))
+
+
+
+mringwgfloor = @model N begin
+    diam ~ Dists.Uniform(25.0, 85.0)
+    fwhm ~ Dists.Uniform(1.0, 50.0)
+    rad = diam/2
+    σ = fwhm/fwhmfac
+
+    ma ~ Dists.Uniform(0.0, 0.5) |> iid(N)
+    mp ~ Dists.Uniform(-1π, 1π) |> iid(N)
+    α = ma.*cos.(mp)
+    β = ma.*sin.(mp)
+
+    #Fraction of floor flux
+    floor ~ Dists.Uniform(0.0, 1.0)
+    dg ~ Dists.Uniform(40.0, 300.0)
+    rg = dg/fwhmfac
+    mring = smoothed(renormed(ROSE.MRing{N}(rad, α, β), (1-floor)), σ)
+    g = renormed(stretched(ROSE.Gaussian(), rg, rg), floor)
+    img = mring + g
+    return img
+end
+
+
+vacp = @model image, uamp, vamp, erramp,
+               u1cp, v1cp, u2cp, v2cp, u3cp, v3cp, errcp begin
+
+    img ~ image
+    vamps = ROSE.visibility_amplitude.(Ref(img), uamp, vamp)
+    amp ~ For(eachindex(vamps, erramp)) do i
+            Dists.Normal(vamps[i], erramp[i])
+    end
+
+    cphases = ROSE.closure_phase.(Ref(img), u1cp, v1cp, u2cp, v2cp, u3cp, v3cp)
+    cphase ~ For(eachindex(cphases, errcp)) do i
+        ROSE.CPVonMises(cphases[i], errcp[i])
+    end
+
+end
+
+
+
+function create_joint_nog(model,
+    ampobs::ROSE.EHTObservation{F,A},
+    cpobs::ROSE.EHTObservation{F,P};
+    amppriors=(AA=0.1,AP=0.1,AZ=0.1,LM=0.2,JC=0.1,PV=0.1,SM=0.1, SP=0.1)
+    ) where {F, A<:ROSE.EHTVisibilityAmplitudeDatum,P<:ROSE.EHTClosurePhaseDatum}
+    uamp = ROSE.getdata(ampobs, :u)
+    vamp = ROSE.getdata(ampobs, :v)
+    bl = ROSE.getdata(ampobs, :baselines)
+    #stations = Tuple(unique(vcat(s1,s2)))
+    #gpriors = values(select(amppriors, stations))
+    erramp = ROSE.getdata(ampobs, :error)
+    amps = ROSE.getdata(ampobs, :amp)
+
+    u1cp = ROSE.getdata(cpobs, :u1)
+    v1cp = ROSE.getdata(cpobs, :v1)
+    u2cp = ROSE.getdata(cpobs, :u2)
+    v2cp = ROSE.getdata(cpobs, :v2)
+    u3cp = ROSE.getdata(cpobs, :u3)
+    v3cp = ROSE.getdata(cpobs, :v3)
+    errcp = ROSE.getdata(cpobs, :error)
+    cps = ROSE.getdata(cpobs, :phase)
+
+    joint = vacp(
+    image=model,
+    uamp=uamp,
+    vamp=vamp,
+    erramp=erramp,
+    u1cp = u1cp,
+    v1cp = v1cp,
+    u2cp = u2cp,
+    v2cp = v2cp,
+    u3cp = u3cp,
+    v3cp = v3cp,
+    errcp = errcp
+    )
+    conditioned = (amp = amps, cphase = cps,)
+    return joint | conditioned
+end
+
+
+function loaddata(imfile, datafile, pa)
+    # Load the image
+    img = ehtim.image.load_image(imfile)
+    img.imvec /= img.total_flux()
+
+    # Assign a particular position angle [need to iterate over some values]
+    img.pa = pa
+
+    # Load the observation
+    obs = ehtim.obsdata.load_uvfits(datafile)
+    img.rf = obs.rf
+    img.ra = obs.ra
+    img.dec = obs.dec
+
+    # Create a synthetic observation to fit
+    obs_fit = img.observe_same(obs, ttype="fast", ampcal=true, phasecal=true, add_th_noise=false)
+    obs_fit.add_amp(debias=true)
+    obs_fit.add_cphase(count="min")
+    damp = ROSESoss.extract_amps(obs_fit)
+    dcp = ROSESoss.extract_cphase(obs_fit)
+    return damp, dcp
+end
+
+function fit_file(imfile, datafile, pa; model=mringwgfloor(N=3,), maxevals=70_000)
+    damp, dcp = loaddata(imfile, datafile, pa)
+    cmg = create_joint_nog(model, damp, dcp)
+    opt, stats = ROSESoss.optimize(BBO(maxevals=maxevals, tracemode=:silent), cmg)
+
+    bl = ROSE.getdata(damp, :baselines)
+    s1 = first.(bl)
+    s2 = last.(bl)
+    stations = Tuple(unique(vcat(s1,s2)))
+    gains = NamedTuple{stations}(Tuple(ones(length(stations))))
+
+    mopt = Soss.predict(cmg.argvals[:image], opt[:img])
+
+    chi2amp = chi2(mopt, damp, gains)/ROSE.nsamples(damp)
+    chi2cp = chi2(mopt, dcp)/ROSE.nsamples(dcp)
+
+    df = (pa = pa, diam = opt.img.diam,
+                   α=opt.img.fwhm,
+                   ff=opt.img.floor,
+                   fwhm_g=opt.img.dg,
+                   amp1 = opt.img.ma[1],
+                   chi2_amp = chi2amp,
+                   chi2_cp = chi2cp)
+    return df
+end
